fbb3d5d831aef23befef8ddcc944632b72c79215
Sindhuja2535 <sindhuja2516@gamil.com>
2025-06-02 14:36:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+app.listen(8080, () =>{
+    console.log("Server Started");
+});
+app.use(cors())
+
+app.get("/", (req,res) => {
+    return res.send("good morning");
+});
+app.get("/greet",(req,res)=>(res.send("greetings")))
+app.get("/name",(req,res)=>(res.send("A.sindhuja")))
+app.get("/weather",(req,res)=>(res.send("30 degree")))
