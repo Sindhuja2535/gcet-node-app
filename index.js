ec15a0547946fa510bb13430e1c903dcc753db9c
Sindhuja2535 <sindhuja2516@gamil.com>
2025-06-05 10:46:14 +0530

    u

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import dotenv from 'dotenv';
+
 import userRouter from "./routes/userRoutes.js";
-
-
-import productRouter from "./routes/productRoutes.js";
+import productRouter from "./routes/ProductRoutes.js";
+//import orderRouter from "./routes/orderRoutes.js";
+import dotenv from "dotenv";
 dotenv.config();
+
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-const MONGO_URI = process.env.MONGO_URI
+const MONGO_URI = process.env.MONGO_URI;
 
 app.use("/users", userRouter);
+
 app.use("/products", productRouter);
-//app.use("/orders",orderRouter)
 
 mongoose
   .connect(MONGO_URI)
